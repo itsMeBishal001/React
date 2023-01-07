9b6199c803555283a0fb9d4e7a20161240130de0
BISHAL DATTA <dattabishal001@gmail.com>
2023-01-07 09:26:25 +0530

    React.createElement to jsx

--- chapter 03/App.js
+++ chapter 03/App.js
@@ -1,33 +1,50 @@
 import React from "react";
-import  ReactDOM  from "react-dom";
+import  ReactDOM  from "react-dom/client";
 
-const heading1=React.createElement(
-    "h1",{
-        key:"h1",
-        id:"h1"
-    },"this is h1"
-);
+// const heading1=React.createElement(
+//     "h1",{
+//         key:"h1",
+//         id:"h1"
+//     },"this is h1"
+// );
 
-const heading2=React.createElement(
-    "h2",{
-        key:"h2",
-        id:"h2"
-    },"this is h2"
-);
+// using jsx
+const heading1 =<h1 key="h1" id ="h1">this is h1</h1>;
 
-const heading3=React.createElement(
-    "h3",{
-        key:"h3",
-        id:"h3"
-    },"this is h3"
-);
-const header=React.createElement(
-    "div",{
-        key:"headings",
-        id:"h",
-        class:"title"
-    },[heading1,heading2,heading3]
-);
+// const heading2=React.createElement(
+//     "h2",{
+//         key:"h2",
+//         id:"h2"
+//     },"this is h2"
+// );
+
+// using jsx
+const heading2 =<h2 key="h2" id ="h2">this is h2</h2>;
+
+// const heading3=React.createElement(
+//     "h3",{
+//         key:"h3",
+//         id:"h3"
+//     },"this is h3"
+// );
+
+// using jsx
+const heading3 =<h3 key="h3" id ="h3">this is h3</h3>;
+
+// const header=React.createElement(
+//     "div",{
+//         key:"headings",
+//         id:"h",
+//         className:"title"
+//     },[heading1,heading2,heading3]
+// );
+
+// using jsx
+const header=<div key="headings" id="h" className="title">
+    {heading1}
+    {heading2}
+    {heading3}
+</div>
 
 const root=ReactDOM.createRoot(document.getElementById('root'));
 root.render(header);
