9b5d27132404501e48b2e9826952e2b6aea807ab
BISHAL DATTA <dattabishal001@gmail.com>
2023-01-07 10:22:06 +0530

    jsx to functional component

--- chapter 03/App.js
+++ chapter 03/App.js
@@ -9,7 +9,8 @@ import  ReactDOM  from "react-dom/client";
 // );
 
 // using jsx
-const heading1 =<h1 key="h1" id ="h1">this is h1</h1>;
+// jsx ==> object ==> HTML(DOM)
+// const heading1 =<h1 key="h1" id ="h1">this is h1</h1>;
 
 // const heading2=React.createElement(
 //     "h2",{
@@ -19,7 +20,8 @@ const heading1 =<h1 key="h1" id ="h1">this is h1</h1>;
 // );
 
 // using jsx
-const heading2 =<h2 key="h2" id ="h2">this is h2</h2>;
+// jsx ==> object ==> HTML(DOM)
+// const heading2 =<h2 key="h2" id ="h2">this is h2</h2>;
 
 // const heading3=React.createElement(
 //     "h3",{
@@ -29,7 +31,8 @@ const heading2 =<h2 key="h2" id ="h2">this is h2</h2>;
 // );
 
 // using jsx
-const heading3 =<h3 key="h3" id ="h3">this is h3</h3>;
+// jsx ==>React.createElement ==> object ==> HTML(DOM)
+// const heading3 =<h3 key="h3" id ="h3">this is h3</h3>;
 
 // const header=React.createElement(
 //     "div",{
@@ -40,11 +43,26 @@ const heading3 =<h3 key="h3" id ="h3">this is h3</h3>;
 // );
 
 // using jsx
-const header=<div key="headings" id="h" className="title">
-    {heading1}
-    {heading2}
-    {heading3}
-</div>
+// jsx ==> object ==> HTML(DOM)
+// const header=<div key="headings" id="h" className="title">
+//     {heading1}
+//     {heading2}
+//     {heading3}
+// </div>
+
+//creating a functional of the same
+const header=()=>{
+    return (
+        <div className="title" key="headerM">
+    <h1 key="h1" id="h1">this is h1</h1>
+    <h2 key="h3" id="h2">this is h2</h2>
+    <h3 key="h3" id="h3">this is h3</h3>
+    </div>
+
+    );
+};
+    
+
 
 const root=ReactDOM.createRoot(document.getElementById('root'));
-root.render(header);
+root.render(header());
